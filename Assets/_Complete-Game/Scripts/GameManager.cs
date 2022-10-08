525d000a29769d1945b53f277627835fb1ddaa8a
Vitor Estevam <vitorestevam02@gmail.com>
2022-10-08 11:39:44 -0300

    Adding Observer pattern to level ui

--- Assets/_Complete-Game/Scripts/GameManager.cs
+++ Assets/_Complete-Game/Scripts/GameManager.cs
@@ -4,7 +4,8 @@ using System.Collections;
 
 namespace Completed
 {
-	using System.Collections.Generic;		//Allows us to use Lists. 
+    using System;
+    using System.Collections.Generic;		//Allows us to use Lists. 
 	using UnityEngine.UI;					//Allows us to use UI.
 	
 	public class GameManager : MonoBehaviour
@@ -15,31 +16,30 @@ namespace Completed
 		public static GameManager instance = null;				//Static instance of GameManager which allows it to be accessed by any other script.
 		[HideInInspector] public bool playersTurn = true;		//Boolean to check if it's players turn, hidden in inspector but public.
 		
-		
-		private Text levelText;									//Text to display current level number.
-		private GameObject levelImage;							//Image to block out level as levels are being set up, background for levelText.
 		private BoardManager boardScript;						//Store a reference to our BoardManager which will set up the level.
 		private int level = 1;									//Current level number, expressed in game as "Day 1".
 		private List<Enemy> enemies;							//List of all Enemy units, used to issue them move commands.
 		private bool enemiesMoving;								//Boolean to check if enemies are moving.
-		private bool doingSetup = true;							//Boolean to check if we're setting up board, prevent Player from moving during setup.
-		
-		
-		
+		private bool doingSetup = true;                         //Boolean to check if we're setting up board, prevent Player from moving during setup.
+
+		public static event Action<int> OnLevelInited;
+		public static event Action OnLevelStarted;
+		public static event Action<int> OnGameOver;
+
 		//Awake is always called before any Start functions
 		void Awake()
 		{
             //Check if instance already exists
             if (instance == null)
 
-                //if not, set instance to this
-                instance = this;
+            //if not, set instance to this
+            instance = this;
 
             //If instance already exists and it's not this:
             else if (instance != this)
 
-                //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
-                Destroy(gameObject);	
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);	
 			
 			//Sets this to not be destroyed when reloading scene
 			DontDestroyOnLoad(gameObject);
@@ -77,38 +77,24 @@ namespace Completed
 			//While doingSetup is true the player can't move, prevent player from moving while title card is up.
 			doingSetup = true;
 			
-			//Get a reference to our image LevelImage by finding it by name.
-			levelImage = GameObject.Find("LevelImage");
-			
-			//Get a reference to our text LevelText's text component by finding it by name and calling GetComponent.
-			levelText = GameObject.Find("LevelText").GetComponent<Text>();
-			
-			//Set the text of levelText to the string "Day" and append the current level number.
-			levelText.text = "Day " + level;
-			
-			//Set levelImage to active blocking player's view of the game board during setup.
-			levelImage.SetActive(true);
-			
-			//Call the HideLevelImage function with a delay in seconds of levelStartDelay.
-			Invoke("HideLevelImage", levelStartDelay);
-			
 			//Clear any Enemy objects in our List to prepare for next level.
 			enemies.Clear();
 			
 			//Call the SetupScene function of the BoardManager script, pass it current level number.
 			boardScript.SetupScene(level);
-			
+
+			//Call the HideLevelImage function with a delay in seconds of levelStartDelay.
+			Invoke("LevelStart", levelStartDelay);
+
+			OnLevelInited?.Invoke(level);
 		}
 		
 		
 		//Hides black image used between levels
-		void HideLevelImage()
+		void LevelStart()
 		{
-			//Disable the levelImage gameObject.
-			levelImage.SetActive(false);
-			
-			//Set doingSetup to false allowing player to move again.
 			doingSetup = false;
+			OnLevelStarted?.Invoke();
 		}
 		
 		//Update is called every frame.
@@ -135,11 +121,7 @@ namespace Completed
 		//GameOver is called when the player reaches 0 food points
 		public void GameOver()
 		{
-			//Set levelText to display number of levels passed and game over message
-			levelText.text = "After " + level + " days, you starved.";
-			
-			//Enable black background image gameObject.
-			levelImage.SetActive(true);
+			OnGameOver?.Invoke(level);
 			
 			//Disable this GameManager.
 			enabled = false;
---
+++ Assets/_Complete-Game/Scripts/UiLevel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Completed
+{
+    public class UiLevel : MonoBehaviour
+    {
+        public Text levelText;
+        private GameObject levelImage;
+
+        private void Awake()
+        {
+            levelText = this.transform.GetChild(0).GetComponent<Text>();
+            levelImage = this.gameObject;
+
+            GameManager.OnLevelInited += HandlerLevelInited;
+            GameManager.OnLevelStarted += handlerLevelStarted;
+            GameManager.OnGameOver += handlerGameOver;
+        }
+
+        private void OnDestroy()
+        {
+            GameManager.OnLevelInited -= HandlerLevelInited;
+            GameManager.OnLevelStarted -= handlerLevelStarted;
+            GameManager.OnGameOver -= handlerGameOver;
+        }
+
+        void HandlerLevelInited(int level) {
+            levelText.text = "Day " + level;
+            levelImage.SetActive(true);
+        }
+
+        void handlerLevelStarted()
+        {
+            levelImage.SetActive(false);
+        }
+
+        void handlerGameOver(int level) {
+            levelText.text = "After " + level + " days, you starved.";
+            levelImage.SetActive(true);
+        }
+    }
+}
